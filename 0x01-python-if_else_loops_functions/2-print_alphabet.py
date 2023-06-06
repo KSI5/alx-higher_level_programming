67faf345d1f6b474de02f88c6b9f67ade7b5c346
KSI5 <krissigebs@gmail.com>
2023-06-06 04:20:23 -0700

    fixed 2-print_alphabet.py file

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-alphabet = ''
-for letter in range(ord('a'), ord('z')+1):
-    alphabet += chr(letter)
-print(alphabet, end='')
+alphabet = ''.join(chr(letter) for letter in range(ord('a'), ord('z')+1))
+print("{}".format(alphabet), end='')
