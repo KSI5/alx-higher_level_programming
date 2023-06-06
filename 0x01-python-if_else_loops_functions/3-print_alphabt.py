018b97e171d37f188c41dbbb3a0ea4a1dbdf083c
KSI5 <krissigebs@gmail.com>
2023-06-06 04:34:24 -0700

    fixed 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
 alphabet = ""
 for letter in range(ord('a'), ord('z')+1):
     if chr(letter) not in 'eq':
         alphabet += chr(letter)
-
 print("{}".format(alphabet), end="")
-
