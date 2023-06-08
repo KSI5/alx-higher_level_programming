6d17aaf88a64c318df8ae484a41cb90979117172
KSI5 <krissigebs@gmail.com>
2023-06-08 05:08:09 -0700

    fixed error in 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
@@ -9,6 +11,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
