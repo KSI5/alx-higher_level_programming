b12e6dcf23322110706072ea1827e399dc545727
KSI5 <krissigebs@gmail.com>
2023-06-14 08:14:38 -0700

    fixed 103-python.c file

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <Python.h>
+#include <stdio.h>
 
 void print_python_list(PyObject *p)
 {
     Py_ssize_t size, i;
     PyObject *item;
 
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
-
     size = PyList_Size(p);
+
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
     for (i = 0; i < size; i++)
     {
         item = PyList_GetItem(p, i);
@@ -21,7 +22,7 @@ void print_python_list(PyObject *p)
 void print_python_bytes(PyObject *p)
 {
     Py_ssize_t size, i;
-    char *string;
+    char *str;
 
     printf("[.] bytes object info\n");
 
@@ -31,15 +32,19 @@ void print_python_bytes(PyObject *p)
         return;
     }
 
-    size = PyBytes_GET_SIZE(p);
-    string = PyBytes_AS_STRING(p);
+    size = PyBytes_Size(p);
+    str = PyBytes_AsString(p);
 
     printf("  size: %ld\n", size);
-    printf("  trying string: %s\n", string);
+    printf("  trying string: %s\n", str);
 
-    printf("  first %ld bytes:", size < 10 ? size + 1 : 10);
-    for (i = 0; i < (size < 10 ? size : 10); i++)
-        printf(" %02x", string[i]);
-
+    printf("  first %ld bytes: ", size + 1);
+    for (i = 0; i < size + 1 && i < 10; i++)
+    {
+        printf("%02hhx", str[i]);
+        if (i == size)
+            break;
+        printf(" ");
+    }
     printf("\n");
 }
