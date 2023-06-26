efc934091316e42bfba34f528ba6c9fd8438212f
KSI5 <krissigebs@gmail.com>
2023-06-26 07:15:44 -0700

    fixed adanced task 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    # Initialize the result variable
     result = 0
-
-    # Iterate from 1 to 3
     for i in range(1, 3):
         try:
-            # Check if i is greater than a
             if i > a:
-                # Raise an exception with the message 'Too far'
                 raise Exception('Too far')
             else:
-                # Calculate the intermediate result
                 result += a ** b / i
-        except:
-            # If an exception occurs, set the result to b + a
+        except Exception as e:
             result = b + a
-            # Exit the loop
             break
-
-    # Return the final result
     return result
