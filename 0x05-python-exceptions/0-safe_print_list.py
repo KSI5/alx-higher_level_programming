31301e4f3a8af0f703bfa2a8cffd4b9c5b21338d
KSI5 <krissigebs@gmail.com>
2023-06-26 04:03:41 -0700

    first comit for task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0
     try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
-            count += 1
-    except IndexError:
-        pass
-    finally:
+        count = 0
+        for item in my_list:
+            if count < x:
+                print(item, end="")
+                count += 1
         print()
-        return count
-
+        return True
+    except Exception:
+        return False
