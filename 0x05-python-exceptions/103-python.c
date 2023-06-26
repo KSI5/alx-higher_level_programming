2fff0d3cdff0946f565dac5bcda35481521ce4b8
KSI5 <krissigebs@gmail.com>
2023-06-26 07:50:36 -0700

    fixed advanced task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,5 +1,9 @@
 #include <Python.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
  * print_python_list - Prints basic info about Python lists.
  * @p: A PyObject list object.
@@ -98,4 +102,3 @@ void print_python_float(PyObject *p)
 	printf("  value: %s\n", buffer);
 	PyMem_Free(buffer);
 }
-
