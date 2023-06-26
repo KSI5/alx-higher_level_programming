09583e66fd67b400597f7593db1bcf19694e971e
KSI5 <krissigebs@gmail.com>
2023-06-26 05:47:11 -0700

    fixed 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
-def raise_exception():
-    try:
-        raise TypeError("This is a type exception")
-    except TypeError as e:
-        print("An error occurred:", str(e))
-        return False
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in my_list:
+        try:
+            if isinstance(i, int):
+                print("{:d}".format(i), end=" ")
+                count += 1
+            if count == x:
+                break
+        except:
+            pass
+    print()
+    return count
