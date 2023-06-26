0f83a826f51ee5500719d7e19db8473b036ed45f
KSI5 <krissigebs@gmail.com>
2023-06-26 04:05:53 -0700

    first comit for task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,7 @@ def safe_print_list(my_list=[], x=0):
                 print(item, end="")
                 count += 1
         print()
-        return True
-    except Exception:
-        return False
+        return count
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return 0
