126e6d34e5b7b8b6902af64839c3a5d5dc13ae06
KSI5 <krissigebs@gmail.com>
2023-07-03 04:14:58 -0700

    fixed 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
 class Rectangle:
-    """Defines a rectangle."""
+    """Represents a rectangle."""
 
     def __init__(self, width=0, height=0):
         """Initializes a rectangle with the given width and height."""
