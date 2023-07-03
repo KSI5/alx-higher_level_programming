430b535886a0d50bd0432d31a5080902a8c9b8f4
KSI5 <krissigebs@gmail.com>
2023-07-03 04:10:55 -0700

    fixed 4-rectangle.py file

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -65,3 +65,11 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
+
+    def __repr__(self):
+        """Return the string representation of the Rectangle.
+
+        Represents the rectangle as a string that can be used to create a new
+        object using eval().
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
