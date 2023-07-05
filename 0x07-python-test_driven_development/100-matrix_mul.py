3840e42e0979f3ced8ce7fad6bb50802180f2958
KSI5 <krissigebs@gmail.com>
2023-07-05 09:56:17 -0700

    fixed 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -10,26 +10,21 @@ def matrix_mul(m_a, m_b):
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
     Raises:
-        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
-        ValueError: If m_a or m_b is an empty list.
-        TypeError: If m_a and m_b have different-sized rows.
+        ValueError: If m_a or m_b is not a valid matrix.
         ValueError: If m_a and m_b cannot be multiplied.
     Returns:
         A new matrix representing the multiplication of m_a by m_b.
     """
 
     if not isinstance(m_a, list) or not isinstance(m_b, list):
-        raise TypeError("Both m_a and m_b must be lists")
+        raise ValueError("Both m_a and m_b must be lists")
 
-    if len(m_a) == 0 or len(m_a[0]) == 0 or len(m_b) == 0 or len(m_b[0]) == 0:
-        raise ValueError("Both m_a and m_b cannot be empty")
+    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
+        raise ValueError("Both m_a and m_b must be lists of lists")
 
-    if any(not isinstance(row, list) for row in m_a) or any(not isinstance(row, list) for row in m_b):
-        raise TypeError("Both m_a and m_b must be lists of lists")
-
-    if any(not all(isinstance(ele, int) or isinstance(ele, float) for ele in row) for row in m_a) or \
-            any(not all(isinstance(ele, int) or isinstance(ele, float) for ele in row) for row in m_b):
-        raise TypeError("Both m_a and m_b should contain only integers or floats")
+    if not all(isinstance(ele, (int, float)) for row in m_a for ele in row) or \
+            not all(isinstance(ele, (int, float)) for row in m_b for ele in row):
+        raise ValueError("Both m_a and m_b should contain only integers or floats")
 
     if any(len(row) != len(m_a[0]) for row in m_a) or any(len(row) != len(m_b[0]) for row in m_b):
         raise ValueError("Rows of m_a and m_b must have the same size")
@@ -37,8 +32,7 @@ def matrix_mul(m_a, m_b):
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b cannot be multiplied")
 
-    # Transpose matrix m_b to simplify multiplication
-    transposed_b = [[m_b[j][i] for j in range(len(m_b))] for i in range(len(m_b[0]))]
+    transposed_b = list(zip(*m_b))
 
     new_matrix = []
     for row_a in m_a:
