2917e3d011aad871c6cdd2aed8ad9e18154d057a
KSI5 <krissigebs@gmail.com>
2023-08-16 06:43:35 -0700

    fixed errors

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- List Privileges of MySQL Users
--- Execute: cat 0-show_grants.sql | mysql -hlocalhost -uroot -p
-
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- This script creates the MySQL server user user_0d_1 with all privileges
 -- Execute: cat 1-create_user.sql | mysql -hlocalhost -uroot -p
-
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
