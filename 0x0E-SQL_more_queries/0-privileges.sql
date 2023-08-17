d82daab5d0bae174747595bfde0105f2515cce46
KSI5 <krissigebs@gmail.com>
2023-08-17 01:04:20 -0700

    fixed errors for tasks 0 & 1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-SHOW_USER_PRIVILEGES_SQL="SHOW GRANTS FOR '$USER_1'@'localhost'; SHOW GRANTS FOR '$USER_2'@'localhost';"
+SHOW GRANTS FOR '$USER_1'@'localhost'; SHOW GRANTS FOR '$USER_2'@'localhost';
