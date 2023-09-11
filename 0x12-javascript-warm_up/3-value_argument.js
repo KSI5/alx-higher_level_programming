ec1b660e49d713555c9bfcdf60a71ac3b354ac3a
KSI5 <krissigebs@gmail.com>
2023-09-11 11:17:21 -0700

    fixed errors

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-
 // Check if the first argument exists and print it, or "No argument" if not
- if (process.argv[2]) {
-   console.log(process.argv[2]);
-   } else {
-     console.log("No argument");
-     }     
+
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
