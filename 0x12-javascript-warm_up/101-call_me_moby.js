8f5721e45c1161801d13e12e4d892884a4acffd8
KSI5 <krissigebs@gmail.com>
2023-09-11 09:44:23 -0700

    fixed errors

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-// function that executes x times a function.
- exports.callMeMoby = function (x, theFunction) {
-   while (x > 0) {
-       theFunction();
-           x--;
-             }
-             };
+// executes x times a function.
+
+exports.callMeMoby = function (x, theFunction) {
+	  for (let i = 0; i < x; i++) {
+		      theFunction();
+		    }
+};
