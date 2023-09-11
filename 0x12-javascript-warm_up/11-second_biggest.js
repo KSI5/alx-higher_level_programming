a07addb76570b5c55912168333a4ba6f884809de
KSI5 <krissigebs@gmail.com>
2023-09-11 12:09:12 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+
 // Find the second largest integer among the provided arguments.
 const mySol = process.argv;
+
 if (mySol.length <= 3) {
-	  console.log(0);
+  console.log(0);
 } else {
-	  console.log(mySol.sort((x, y) => y - x).slice(3)[0]);
+  console.log(mySol.sort((x, y) => y - x).slice(3)[0]);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,10 @@
 const size = parseInt(process.argv[2]);
 
 if (isNaN(size) || size <= 0) {
-	  console.log("Missing size");
+  console.log('Missing size');
 } else {
-	  const row = 'X'.repeat(size);
-	  for (let i = 0; i < size; i++) {
-		      console.log(row);
-		    }
+  const row = 'X'.repeat(size);
+  for (let i = 0; i < size; i++) {
+    console.log(row);
+  }
 }
