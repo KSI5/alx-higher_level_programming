62601fa1a031150d428e68b0777799f25ff762b9
KSI5 <krissigebs@gmail.com>
2023-09-15 07:16:12 -0700

    Updated file

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,43 +1,25 @@
 #!/usr/bin/python3
 """
-Script that changes the name of a State object from the database hbtn_0e_6_usa
+Script that changes the name of a State object to the database
+Using module SQLAlchemy
 """
+
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
-    # Check if all three arguments are provided
-    if len(argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(argv[0]))
-        exit(1)
-
-    # Extract the arguments
-    mysql_username = argv[1]
-    mysql_password = argv[2]
-    database_name = argv[3]
-
-    # Create a connection to the MySQL database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        mysql_username, mysql_password, database_name))
-
-    # Create a configured "Session" class
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
-
-    # Create a Session
+    # create a Session
     session = Session()
-
-    # Query the State object with id=2
-    state_to_update = session.query(State).filter_by(id=2).first()
-
-    if state_to_update:
-        # Update the name of the State to "New Mexico"
-        state_to_update.name = "New Mexico"
-        # Commit the changes to the database
-        session.commit()
-    else:
-        print("State with id=2 not found")
-
-    # Close the session
+    Base.metadata.create_all(engine)
+    state_update = session.query(State).filter_by(id='2').first()
+    state_update.name = "New Mexico"
+    # commit and close session
+    session.commit()
     session.close()
