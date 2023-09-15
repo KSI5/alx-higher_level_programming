8302d32db92b4f05d6ab806e059dfb1eb79bd419
KSI5 <krissigebs@gmail.com>
2023-09-15 03:01:00 -0700

    Committing local changes

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-/* Update this script by adding a new function
- * incr that increments the integer value. */
+//  new function incr that increments the integer value.
 const myObject = {
 	  type: 'object',
 	  value: 12
 };
 console.log(myObject);
 myObject.incr = function () {
-	  this.value++;
+	  myObject.value++;
 };
 myObject.incr();
 console.log(myObject);
