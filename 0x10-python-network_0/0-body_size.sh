c62f049e54193a91b75caeacc7ba1e0edd117f3c
KSI5 <krissigebs@gmail.com>
2023-09-28 03:50:13 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL and displays the size of the response body in bytes.
-
-# Send a GET request to the provided URL and use curl's -s (silent) and -o (output) options
-curl -s -o /tmp/body_size_response "$1"
-
-# Use the 'stat' command to get the size of the downloaded file (response body)
-body_size=$(stat -c %s /tmp/body_size_response)
-
-# Display the size in bytes
-echo "$body_size"
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
