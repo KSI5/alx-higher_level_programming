903869a3cfa65cd79b112e96bbf587babcbce249
KSI5 <krissigebs@gmail.com>
2023-09-28 05:28:30 -0700

    Fixed 6-peak.py file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-''' Find peak in a list of unsorted integers '''
+"""find the max"""
+
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
+    """find_peak"""
+    if not len(list_of_integers):
         return None
-
-    left, right = 0, len(list_of_integers) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            right = mid
-        else:
-            left = mid + 1
-
-    return list_of_integers[left]
+    else:
+        return max(list_of_integers)
