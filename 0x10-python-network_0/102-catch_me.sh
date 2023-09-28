1b18132473ae1850e5e2415c1c48c7821fb96c3d
KSI5 <krissigebs@gmail.com>
2023-09-28 05:41:49 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me and extracts the message
-
-# Send the request and write the response to a file
-curl -s 0.0.0.0:5000/catch_me -o response.txt
-
-# Use grep to extract the message
-grep -o "You got me!" response.txt
+curl -sL -X PUT -d "user_id=98" -H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
