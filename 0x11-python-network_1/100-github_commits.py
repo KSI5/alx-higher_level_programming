ff5c0c756e3f14ae5272da064a439b4cc35ac629
KSI5 <krissigebs@gmail.com>
2023-09-29 09:15:09 -0700

    Advanced Task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,33 +7,17 @@ of the repository specified by the user.
 import sys
 import requests
 
+
 if __name__ == "__main__":
-    # Check if both repository name and owner name arguments were provided
-    if len(sys.argv) != 3:
-        sys.exit("Usage: ./list_commits.py <repository_name> <owner_name>")
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    repo_name = sys.argv[1]
-    owner = sys.argv[2]
-    url = f'https://api.github.com/repos/{owner}/{repo_name}/commits'
-
+    r = requests.get(url)
+    commits = r.json()
     try:
-        request = requests.get(url)
-        request.raise_for_status()  # Raise an exception for HTTP errors
-        commits = request.json()
-
-        # Ensure there are at least 10 commits
-        if len(commits) < 10:
-            print("Less than 10 commits available in the repository.")
-        else:
-            # List the 10 most recent commits
-            for i in range(10):
-                sha = commits[i]['sha']
-                author_name = commits[i]['commit']['author']['name']
-                print('{}: {}'.format(sha, author_name))
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-
-    except ValueError:
-        print("Invalid JSON response from GitHub API")
-
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
