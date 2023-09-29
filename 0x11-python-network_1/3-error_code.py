c2a98bda96ac252f583ff58606b6297ce808121b
KSI5 <krissigebs@gmail.com>
2023-09-29 08:25:47 -0700

    Mandatory Tasks

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-A Python script that takes a URL as input, sends a request to the URL, and displays
-the response body (decoded in utf-8).
+A Python script that takes a URL as input, sends a request to
+the URL, and display the response body (decoded in utf-8).
 
 Requirements:
 - You must use the packages urllib and sys.
-- Handle urllib.error.HTTPError exceptions and print the associated HTTP status code.
+- Handle urllib.error.HTTPError exceptions and print
+- the associated HTTP status code.
 
 Usage:
 ./3-error_code.py <URL>
@@ -20,6 +21,6 @@ if __name__ == "__main__":
         with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as error:
-        print("Error code: {}".format(error.code))
-    except:
+        print(f"Error code: {error.code}")
+    except Exception as e:
         pass
