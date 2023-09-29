d080529559c950fc46475b354060c4ff8f2a0494
KSI5 <krissigebs@gmail.com>
2023-09-29 07:49:38 -0700

    Mandatory Tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,11 @@ import urllib.request
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    try:
+        with urllib.request.urlopen(url) as response:
+            print("Body response:")
+            print("\t- type:", type(response.read()))
+            print("\t- content:", response.read())
+            print("\t- utf8 content:", response.read().decode("utf-8"))
+    except Exception as e:
+        print(e)
