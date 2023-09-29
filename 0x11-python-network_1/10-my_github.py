ff0943a9d13e7d72423d17a957136eeacc9af5e6
KSI5 <krissigebs@gmail.com>
2023-09-29 09:11:04 -0700

    Mandatory Tasks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-A Python script that takes GitHub credentials (username and personal access token)
-and uses Basic Authentication to access the GitHub API and display your GitHub User ID.
+A Python script that takes GitHub credentials
+(username and personal access token)
+and uses Basic Authentication to access the
+GitHub API and display your GitHub User ID.
 
 Requirements:
 - Use the requests package.
@@ -16,35 +18,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    # Check if both username and personal access token arguments were provided
-    if len(sys.argv) != 3:
-        sys.exit("Usage: ./github_user_id.py <username> <personal_access_token>")
-
-    username = sys.argv[1]
-    personal_access_token = sys.argv[2]
-
-    # Construct the GitHub API URL to fetch user information
-    url = f"https://api.github.com/user"
-
-    # Create Basic Authentication
-    auth = HTTPBasicAuth(username, personal_access_token)
-
-    try:
-        # Send a GET request to the GitHub API
-        response = requests.get(url, auth=auth)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        user_data = response.json()
-
-        # Check if the response contains user information
-        if 'id' in user_data:
-            user_id = user_data['id']
-            print(f"GitHub User ID: {user_id}")
-        else:
-            print("User information not found")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-
-    except ValueError:
-        print("Invalid JSON response from GitHub API")
-
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
