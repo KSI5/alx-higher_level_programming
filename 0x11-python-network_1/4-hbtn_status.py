f8398ca8c9beba2b274a994d86ca2c929eb4d215
KSI5 <krissigebs@gmail.com>
2023-09-29 08:48:49 -0700

    Mandatory Tasks

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,11 +5,9 @@ https://alx-intranet.hbtn.io/status using the requests package
 and displays the body of the response with tabulation.
 """
 
-import requests
-
-
-if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
+if __name__ == '__main__':
+    import requests
+    html = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
